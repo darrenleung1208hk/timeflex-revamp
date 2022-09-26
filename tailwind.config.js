384b46a8d9bd746f49e0fee7c890317a321ca42a
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-26 18:08:58 +0800

    added primary key colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
+		colors: {
+			primary: "#1891D7",
+			onPrimary: "#FFFFFF",
+			primaryContainer: "#D8EEFB",
+			onPrimaryContainer: "#0C4A6E",
+		},
 		extend: {},
 	},
 	plugins: [],
