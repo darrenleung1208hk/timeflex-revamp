a76984b75b630ed2a0c7e9e390d23f7a31410a09
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-09-30 22:21:38 +0800

    return correct number of days depending on the number of weeks in a given month

--- core/calendar/utils.ts
+++ core/calendar/utils.ts
@@ -1,10 +1,11 @@
 import { Month } from "./interface";
-import { addDays, set, startOfWeek } from "date-fns";
+import { addDays, set, startOfWeek, getWeeksInMonth } from "date-fns";
 
 export const getMonthCalendarDays = (month: Month): Date[] => {
 	const firstDayOfMonth = set(new Date(), { month, date: 1 });
+	const numberOfWeeks = getWeeksInMonth(firstDayOfMonth);
 	const firstMonthCalendarDay = startOfWeek(firstDayOfMonth);
-	return new Array(35)
+	return new Array(numberOfWeeks * 7)
 		.fill(firstMonthCalendarDay)
 		.map((day, index) => addDays(day, index));
 };
