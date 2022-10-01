2399a88ccfb7d1d2905ed632a60bf409108458f4
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-10-01 17:24:34 +0800

    added view select

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,3 +1,4 @@
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
 import KeyboardArrowLeftRoundedIcon from "@mui/icons-material/KeyboardArrowLeftRounded";
 import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRightRounded";
 import {
@@ -5,12 +6,28 @@ import {
 	Box,
 	Button,
 	IconButton,
+	ListItemText,
+	Menu,
+	MenuItem,
 	Stack,
 	Toolbar,
 	Typography,
 } from "@mui/material";
+import { MouseEvent, useState } from "react";
+
+const views = ["day", "week", "month"];
 
 export const AppBar = () => {
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+	const toggleViewSelect = (e: MouseEvent<HTMLElement>) => {
+		if (Boolean(anchorEl)) {
+			setAnchorEl(null);
+		} else {
+			setAnchorEl(e.currentTarget);
+		}
+	};
+
 	return (
 		<div>
 			<Toolbar />
@@ -22,6 +39,38 @@ export const AppBar = () => {
 					>
 						TimeFlex
 					</Typography>
+					<Stack direction="row" spacing={2}>
+						<Button
+							endIcon={<KeyboardArrowDownRoundedIcon />}
+							onClick={toggleViewSelect}
+							variant={Boolean(anchorEl) ? "contained" : "text"}
+						>
+							Week
+						</Button>
+						<Menu
+							open={Boolean(anchorEl)}
+							anchorEl={anchorEl}
+							onClose={() => setAnchorEl(null)}
+							PaperProps={{
+								sx: {
+									minWidth: "200px",
+									borderRadius: 0,
+								},
+							}}
+						>
+							{views.map((view) => (
+								<MenuItem key={view}>
+									<ListItemText
+										primary={view}
+										primaryTypographyProps={{
+											variant: "body2",
+											textTransform: "capitalize",
+										}}
+									/>
+								</MenuItem>
+							))}
+						</Menu>
+					</Stack>
 					<Stack direction="row" spacing={2}>
 						<Box>
 							<IconButton color="primary">
